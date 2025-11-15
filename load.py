3f17fae2916e56085036947c215e8c598231a0aa
codebynishan <mail.nishanrana@gmail.com>
2025-11-15 15:40:42 +0545

    load data to database

---
+++ load.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from sqlalchemy import create_engine
+
+
+df = pd.read_csv("cleaned_data.csv")
+
+
+user = "postgres"
+password = "Nishan%402059"
+
+host = "localhost"
+port = "5432"
+database = "superstore_db"
+
+
+engine = create_engine(f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}")
+
+
+table_name = "superstore"
+
+
+df.to_sql(table_name, con=engine, if_exists='replace', index=False)
+
+print(f"Data loaded successfully into table '{table_name}'")
